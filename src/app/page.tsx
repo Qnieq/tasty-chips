d351cc43a011352b18fee61b662999324271f1f3
Mike <misha.o.son@gmail.com>
2025-01-29 05:45:09 +0300

    added analitics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Home } from "./Home";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center w-full h-full min-h-[100dvh]">
       <Home />
+      <Analytics />
     </main>
   );
 }
- 
